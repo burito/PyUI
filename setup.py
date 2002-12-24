276dc4e2de7a0abe45f5424cc01197c11d593661
mrriley <mrriley>
2002-12-24 02:30:22 +0000

    added setup.py file to install with distutils

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(name="PyUI",
+      version="0.93",
+      description="Python User Interface Library",
+      author="Sean Riley",
+      author_email="sean@twistedmatrix.com",
+      url="http://pyui.sf.net",
+      packages=['pyui','pyui.renderers','pyui.themes']
+      )
+
+      
